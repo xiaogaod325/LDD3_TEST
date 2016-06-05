72702486bbba793f3b8c1da656f44d2f6174e547
xiaogaod325 <1135143763@qq.com>
2016-06-05 18:06:31 +0800

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   scull.c

---
+++ scull.c
@@ -0,0 +1,21 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kdev_t.h>
+MODULE_LICENSE("Dual BSD/GPL");
+#define SCULL_MAJOR 100
+#define SCULL_MINOR 1
+dev_t dev_num=MKDEV(SCULL_MAJOR,SCULL_MINOR);
+static int hello_init(void)
+{
+    printk(KERN_ALERT "hello,world");
+    return 0;
+}
+
+static void hello_exit(void)
+{
+    printk(KERN_ALERT "bye,world");
+
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
