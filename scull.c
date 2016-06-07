fc041d2321528b90f50815cfc6b5a7193ee8613a
xiaogaod325 <1135143763@qq.com>
2016-06-07 22:22:49 +0800

    modified:   scull.c 	modified:   scull_load

--- scull.c
+++ scull.c
@@ -1,9 +1,10 @@
 #include<linux/init.h>
+#include<linux/fs.h>
 #include<linux/module.h>
 #include<linux/kdev_t.h>
 MODULE_LICENSE("Dual BSD/GPL");
 #define SCULL_MAJOR 100
-#define SCULL_MINOR 1
+#define SCULL_MINOR 0
 int i=0;
 
 EXPORT_SYMBOL(i);
@@ -11,12 +12,18 @@ EXPORT_SYMBOL(i);
 dev_t dev_num=MKDEV(SCULL_MAJOR,SCULL_MINOR);
 static int hello_init(void)
 {
+    int result;
     printk(KERN_ALERT "hello,world");
+    if(SCULL_MAJOR)
+       	result=register_chrdev_region(dev_num,4,"scull");
+    else 
+        result=alloc_chrdev_region(&dev_num,SCULL_MINOR,4,"scull");
+    
+     
+    
     return 0;
 }
 
-...gao :]
-....wang
 
 static void hello_exit(void)
 {
