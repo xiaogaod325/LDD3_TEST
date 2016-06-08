c97d663aa986bce2345758ec7e860cf263b57b3c
=xiaogaod325 <1135143763@qq.com>
2016-06-08 17:14:55 +0800

    modified:   scull.c 	modified:   scull_load

--- scull.c
+++ scull.c
@@ -9,6 +9,54 @@ int i=0;
 
 EXPORT_SYMBOL(i);
  
+loff_t scull_llseek(struct file * filep,loff_t off,int whence)
+{
+
+    return 0;
+}  
+
+int scull_open(struct inode * inode,struct file * filep)
+{
+    return 0;
+
+}
+
+
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+ 
+ {
+ return 0;
+ }
+
+int scull_write(struct file *filp,const char __user *buff,size_t count,loff_t *f_off)
+{
+    return 0;
+
+}
+
+long scull_ioctl(struct file *filp,unsigned int cmd,unsigned long arg)
+ {
+    return 0;
+ 
+ }
+ 
+ int scull_release(struct inode *inode,struct file *filp)
+ {
+    return 0;
+ 
+ }
+ 
+ struct file_operations fops={
+    .owner =THIS_MODULE,
+    .llseek = scull_llseek,
+    .read   = scull_read,
+    .write  = scull_write,
+   .unlocked_ioctl  = scull_ioctl,
+    .open   = scull_open,
+    .release= scull_release,
+    };
+ 
+ 
 dev_t dev_num=MKDEV(SCULL_MAJOR,SCULL_MINOR);
 static int hello_init(void)
 {
@@ -18,8 +66,13 @@ static int hello_init(void)
        	result=register_chrdev_region(dev_num,4,"scull");
     else 
         result=alloc_chrdev_region(&dev_num,SCULL_MINOR,4,"scull");
-    if(result != 0)
+    if(result != 0){
         printk("scull register dev number fail \n");   
+        return result;
+     }
+     
+     
+     
      
     
     return 0;
@@ -28,6 +81,7 @@ static int hello_init(void)
 
 static void hello_exit(void)
 {
+    unregister_chrdev_region(dev_num,4);
     printk(KERN_ALERT "bye,world");
 
 }
